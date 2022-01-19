c939e78f6d1780f17dca55e8ab820a6c7adef90a
MaxieEs <estheredemewok01@gmail.com>
2022-01-19 17:57:34 +0100

    added templates and index function

--- Regression/views.py
+++ Regression/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django import forms
+
+class Predict_form(forms.Form):
+    Quantity = forms.IntegerField(min_value=1)
+    Price = forms.DecimalField(min_value=1)
+
+def Index(request):
+    form = Predict_form()
+    return render(request, 'Regression/index.html', {'form' : form})
+
